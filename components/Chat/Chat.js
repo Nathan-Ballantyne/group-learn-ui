eb216b30261c119d9a79323306ba91011ad3507d
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2021-01-10 17:54:11 +1300

    add chat window component - WIP

---
+++ components/Chat/Chat.js
@@ -0,0 +1,11 @@
+import styles from './Chat.module.scss';
+
+const Chat = () => {
+  return (
+    <div className={styles.Chat}>
+      <div className={styles.textBar}></div>
+    </div>
+  );
+};
+
+export default Chat;
