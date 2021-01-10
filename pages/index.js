88994dfeafe852101e91befe09b387314fd0641b
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2021-01-10 17:56:26 +1300

    update to use the document component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
-import SideNav from '../components/SideNav';
+import Document from '../components/Document';
 import GroupList from '../components/GroupsList';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Group Learn</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <SideNav />
+      <Document pageTitle='Group Learn' />
 
       <main className={styles.main}>
         <h1 className={styles.title}>Browse Groups</h1>
